96d93556dba2249e01410635cc97abe791686b5c
Admin <Admin@DESKTOP-9SFIQ24.bbrouter>
2024-06-24 20:48:13 +0530

    Commiting

---
+++ mavenproject/src/main/java/accessmodifier/AccModifier2.java
@@ -0,0 +1,16 @@
+package accessmodifier;
+
+public class AccModifier2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccModifier obj=new AccModifier();
+		
+		obj.method1();
+		/*obj.method2();*/
+		obj.method();
+		obj.method3();
+
+	}
+
+}
